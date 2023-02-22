e446d394b0efa6d072bc39e296b1747c0003944b
sarah <sarah.annealice@gmail.com>
2023-02-22 14:44:01 -0400

    added quiz file, work on file IO

--- app/src/main/java/com/example/quizbuilderapp/Questions.java
+++ app/src/main/java/com/example/quizbuilderapp/Questions.java
@@ -3,22 +3,31 @@ package com.example.quizbuilderapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.*;
 import android.widget.*;
 
 //java specific imports
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.util.HashMap;
 
 public class Questions extends AppCompatActivity {
     //views
     Button opt1Btn, opt2Btn, opt3Btn, opt4Btn;
     Button nextBtn;
-    static TextView questionTv, progressTv;
+    TextView questionTv, progressTv;
 
     //variables
     static int counter = 0;
-    static ArrayList<String> definitions = new ArrayList<>();
-    static ArrayList<String> terms = new ArrayList<>();
+    final String TAG = "Error";
+    static ArrayList<String> questions = new ArrayList<>();
+    static ArrayList<String> answers = new ArrayList<>();
     //hashmap variable
 
     @Override
@@ -37,14 +46,15 @@ public class Questions extends AppCompatActivity {
         questionTv = findViewById(R.id.questionTv);
         progressTv = findViewById(R.id.progressTv);
 
-        //if statement to check if file is empty
-        nextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent resultsIntent = new Intent(Questions.this, Results.class);
-                startActivity(resultsIntent);
-            }
-        });
+        answers.add("red");
+        answers.add("orange");
+        answers.add("yellow");
+        answers.add("pink");
+
+        questions.add("mixed with blue creates purple");
+        questions.add("the complementary colour to blue");
+        questions.add("a primary colour with six letters");
+        questions.add("the colour associated with love");
     }//end onCreate
 
     //onClick for answers
@@ -66,19 +76,20 @@ public class Questions extends AppCompatActivity {
                     break;
             }
         }
-    };
+    };//end onClick for options
 
     //onClick for next
-//    public View.OnClickListener nextBtnClicked = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            //if statement to check whether to go to results or not
-////            if (counter == 10) {
-////                Intent resultsIntent = new Intent(getBaseContext(), Results.class);
-////                startActivity(resultsIntent);
-//            //} else {
-//                setContentView(R.layout.activity_results);
-//            //}
-//        }
-//    };
+    public View.OnClickListener nextBtnClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //if statement to check if file is empty
+            if (counter > 10) {
+                Intent resultsIntent = new Intent(Questions.this, Results.class);
+                startActivity(resultsIntent);
+            } else {
+                //randomize questions/answers
+            }
+
+        }
+    };
 }//end question class
---
+++ app/src/main/java/com/example/quizbuilderapp/QuizOperations.java
@@ -0,0 +1,5 @@
+package com.example.quizbuilderapp;
+
+
+public class QuizOperations {
+}
